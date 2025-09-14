2d0b89d9ebdcc8170d1cf4035c3d598f80e764af
Albert Gil Escura <agilescura@gmail.com>
2025-09-14 21:34:59 +0200

    First question

--- Sources/swift-experiments-questionnaire/swift_experiments_questionnaire.swift
+++ Sources/swift-experiments-questionnaire/swift_experiments_questionnaire.swift
@@ -1,22 +1,28 @@
 import SwiftUI
 
-struct Question {
-  let title: String
-  let options: [Answer]
-}
-
-struct Answer: Identifiable, Equatable {
+struct Question: Sendable {
   let id: Int
   let title: String
+  let options: [Option]
+  
+  struct Option: Identifiable, Equatable {
+    let id: Int
+    let title: String
+  }
+}
+
+struct Answer: Identifiable {
+  let id: Int
+  let optionId: Int
 }
 
 struct QuestionView: View {
   private let question: Question
-  @State private var selected: Answer? = nil
+  @State private var selected: Question.Option? = nil
   
   init(
     question: Question,
-    selected: Answer? = nil
+    selected: Question.Option? = nil
   ) {
     self.question = question
     self._selected = State(initialValue: selected)
@@ -44,17 +50,72 @@ struct QuestionView: View {
   }
 }
 
-#Preview {
-  let selectedAnswer = Answer(id: 2, title: "This is the second answer")
+struct FinalView: View {
+  private let questions: [Question]
+  private let answers: [Answer]
+  
+  init(
+    questions: [Question],
+    answers: [Answer]
+  ) {
+    self.questions = questions
+    self.answers = answers
+  }
+  
+  func title(for answer: Answer) -> String {
+    guard let question = self.questions.first(where:{ $0.id == answer.id })
+    else { return "" }
+    return question.title
+  }
+  
+  func description(for answer: Answer) -> String {
+    guard let question = self.questions.first(where:{ $0.id == answer.id }),
+          let option = question.options.first(where:{ $0.id == answer.optionId })
+    else { return "" }
+    return option.title
+  }
+  
+  var body: some View {
+    VStack {
+      ForEach(answers) { answer in
+        Text(title(for: answer))
+        Text("Answered: \(description(for: answer))")
+      }
+    }
+  }
+}
+
+#Preview("Question") {
+  let selectedAnswer = Question.Option(id: 2, title: "This is the second answer")
   QuestionView(
     question: Question(
+      id: 1,
       title: "This is a question",
       options: [
-        Answer(id: 1, title: "This the first answer"),
+        Question.Option(id: 1, title: "This the first answer"),
         selectedAnswer,
-        Answer(id: 3, title: "This is the third ansert")
+        Question.Option(id: 3, title: "This is the third ansert")
       ]
     ),
     selected: selectedAnswer
+  )
+}
+
+#Preview("Final") {
+  FinalView(
+    questions: [
+      Question(
+        id: 1,
+        title: "This is a question",
+        options: [
+          Question.Option(id: 1, title: "This the first answer"),
+          Question.Option(id: 2, title: "This is the second answer"),
+          Question.Option(id: 3, title: "This is the third ansert")
+        ]
+      )
+    ],
+    answers: [
+      Answer(id: 1, optionId: 3)
+    ]
   )
 }
